e38113ed1c23be8349679ccd09e3687337771e19
Muhammad6196 <65651659+Muhammad6196@users.noreply.github.com>
2023-04-28 02:06:18 +0500

    Update eatable.dart

--- foodorder/eatable.dart
+++ foodorder/eatable.dart
@@ -18,12 +18,34 @@ class Eatables {
     '4': "Pepsi",
     '5': "Sprite",
     '6': "Lassi",
-    '7': "Limka",
+    '7': "Limka Drink",
     '8': "Faluda",
     '9': "Watermelon Juice",
     '10': "Mango Shake"
   };
 
+  Map<String, int> prices = {
+    "Zinger Burger": 250,
+    "Pizza": 700,
+    "Broast": 350,
+    "Shawarma": 100,
+    "Karahi": 1250,
+    "Malai Boti": 600,
+    "Chicken Tikka": 300,
+    "BBQ": 350,
+    "French Fries": 100,
+    "Chapli Kabab": 40,
+    "Coffee": 120,
+    "Tea": 60,
+    "Green Tea": 70,
+    "Pepsi": 70,
+    "Sprite": 70,
+    "Lassi": 100,
+    "Limka Drink": 35,
+    "Faluda": 100,
+    "Watermelon Juice": 50,
+    "Mango Shake": 150
+  };
   void show_foodlist() {
     print("*******");
     print("*Foods*");
